7f8a5e267bf0783f3950eed5b66aa771eee2fa52
Dinesh9418 <dineshsjadhav9418@gmail.com>
2024-09-05 11:54:12 +0530

    POST API hit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { apiError } from "../utils/apiError.js";
-import { apiResponse } from "../utils/apiResponse.js";
-import { uploadOnCloudinary } from "../utils/fileUpload.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/fileUpload.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (existedUser) {
-    throw new apiError(409, "User with email or username already exists");
+    throw new ApiError(409, "User with email or username already exists");
   }
   //console.log(req.files);
 
@@ -46,15 +46,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   if (!avatarLocalPath) {
-    throw new apiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar file is required");
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
-    throw new apiError(400, "Avatar file is required..");
+    throw new ApiError(400, "Avatar file is required");
   }
+
   const user = await User.create({
     fullName,
     avatar: avatar.url,
@@ -69,16 +70,12 @@ const registerUser = asyncHandler(async (req, res) => {
   );
 
   if (!createdUser) {
-    throw new apiError(500, "Something went wrong while registering the user");
+    throw new ApiError(500, "Something went wrong while registering the user");
   }
 
   return res
     .status(201)
-    .json(new apiResponse(200, createdUser, "User registered Successfully"));
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
-
-// if (fullName === "") {
-//   throw new apiError(400, "FullName is required");
-// }
 
 export { registerUser };
--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -1,31 +1,32 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
+const a = "dwny8ltbu";
+cloudinary.config({
+  //   cloud_name: process.env.cloudinary_CLOUD_NAME,
+  //   api_key: process.env.cloudinary_API_KEY,
+  //   api_secret: process.env.cloudinary_SECRET_KEY,
 
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+  cloud_name: a,
+  api_key: "744539778327439",
+  api_secret: "mUZImI5g8RPNMyCuwC17Yb3f5XY",
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
-    }
-}
-
-
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
